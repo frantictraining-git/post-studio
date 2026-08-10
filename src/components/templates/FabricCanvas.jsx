7b4ba4527a452ecdfdf23e77f6ffa4461b742e1f
frantictraining-git <frantictraining@gmail.com>
2026-08-10 02:09:05 +0100

    Hotfix: Add missing useState import to FabricCanvas.jsx

--- src/components/templates/FabricCanvas.jsx
+++ src/components/templates/FabricCanvas.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as fabric from 'fabric';
 import { getOverlayById } from '../../assets/overlays';
 
